da6e6ebd7ecd29aa920cb6f71b5cd71b8f4e9fe7
Jonatas Almeida <jonatascalmeida@gmail.com>
2023-01-04 19:51:08 -0300

    initial commit spring app

---
+++ src/main/java/com/jonatasalmeidadev/angularcrudapi/AngularCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.jonatasalmeidadev.angularcrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonatasalmeidadev/angularcrudapi/AngularCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonatasalmeidadev.angularcrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
